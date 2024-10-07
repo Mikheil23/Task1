9139faf27eab0a861d2c23924f0236d911a61c20
Mikheili <mishiko.shengelia98@gmail.com>
2024-10-07 23:04:32 +0400

    changes 3 tasks

--- Satestofaili2#/Program.cs
+++ Satestofaili2#/Program.cs
@@ -1,28 +1,70 @@
-﻿#region Task1
-Console.Write("Please enter circle radius: ");
-var r = Convert.ToDouble(Console.ReadLine());
-var bigSquareSide = r * 2;
-var bigSquareArea = Math.Pow(bigSquareSide, 2);
-Console.WriteLine($"Big square area is: {bigSquareArea:F2}"); 
-var smallSquareSide = r * Math.Sqrt(2);
-var smallSquareArea = Math.Pow(smallSquareSide, 2);
-Console.WriteLine($"Small square area is: {smallSquareArea:F2}"); 
-Console.WriteLine($"Difference between Big and Small square areas is: {bigSquareArea - smallSquareArea:F2}");
-#endregion
-#region Task3
-var x = 3;
-var y = 1;
-var z = 0;
-var win = x;
-var draw = y;
-var lost = z;
-Console.WriteLine("Please write how many matches your club won?");
-var x1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Please write how many matches your club draw?");
-var x2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Please write how many matches your club lost?");
-var f1 = win * x1;
-var f2 = draw * x2;
-var f3 = f1 + f2;
-Console.WriteLine($"Your team has {f3} points");
-#endregion
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Task 1: Calculate the area difference between the big and small squares
+        CalculateSquareAreaDifference();
+
+        // Task 2: Check if the user hit the jackpot
+        CheckJackpot();
+
+        // Task 3: Calculate the total points for a football team
+        CalculateFootballPoints();
+    }
+
+    static void CalculateSquareAreaDifference()
+    {
+        Console.WriteLine("Please enter circle radius:");
+        var r = Convert.ToDouble(Console.ReadLine());
+        var bigSquareSide = r * 2;
+        var bigSquareArea = Math.Pow(bigSquareSide, 2);
+        Console.WriteLine($"Big square area is: {bigSquareArea:F2}"); // F2 გვეხმარება რიცხვი ჩავწეროთ როგორც: 50.00 (ბოლოშო ორზე მეტი ნული არ იქნება.
+        var smallSquareSide = r * Math.Sqrt(2);
+        var smallSquareArea = Math.Pow(smallSquareSide, 2);
+        Console.WriteLine($"Small square area is: {smallSquareArea:F2}");
+        Console.WriteLine($"Difference between Big and Small square areas is: {bigSquareArea - smallSquareArea:F2}");
+    }
+    static void CheckJackpot()
+    {      List<string> Jackpotlist = new List<string>();
+        Console.WriteLine("Please enter symbols (enter an empty line to finish):");
+        while (true)
+        {
+            var input = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(input))
+                Jackpotlist.Add(input);
+            else
+                break;
+        }
+        Dictionary<string, int> uniquesymbols = new Dictionary<string, int>();
+        foreach (var symbol in Jackpotlist)
+        {
+            if (!uniquesymbols.ContainsKey(symbol))
+                uniquesymbols[symbol] = 1;
+            else
+                uniquesymbols[symbol]++;
+        }
+        if (uniquesymbols.Count == 1 && uniquesymbols.First().Value == Jackpotlist.Count)
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+    static void CalculateFootballPoints()
+    {
+        const int pointsPerWin = 3;
+        const int pointsPerDraw = 1;
+        Console.WriteLine("Please write how many matches your club won:");
+        int wins = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please write how many matches your club drew:");
+        int draws = Convert.ToInt32(Console.ReadLine());
+        int totalPoints = (pointsPerWin * wins) + (pointsPerDraw * draws);
+        Console.WriteLine($"Your team has {totalPoints} points");
+    }
+}
--- Satestofaili2#/obj/Debug/net8.0/Satestofaili2#.AssemblyInfo.cs
+++ Satestofaili2#/obj/Debug/net8.0/Satestofaili2#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Satestofaili2#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61d5e1089f2934634e6c522bb75df38a5ac8bf6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58443d832627ed931c5b548f43c0bc2d3b773106")]
 [assembly: System.Reflection.AssemblyProductAttribute("Satestofaili2#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Satestofaili2#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
