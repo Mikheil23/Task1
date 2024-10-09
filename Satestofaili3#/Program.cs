63ecea19294841b0fe29d5ac88330598b32c4612
Mikheili <mishiko.shengelia98@gmail.com>
2024-10-09 17:08:28 +0400

    third file

---
+++ Satestofaili3#/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("What is number a (Minimum number)?");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("What is number b (Maximum number)?");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("What is number n?");
+        int n = int.Parse(Console.ReadLine());
+
+        var result = Task1(a, b, n);
+        Console.WriteLine(result);
+    }
+
+    private static int Task1(int a, int b, int n)
+    {
+        int count = 0;
+        int baseNum = 1;
+
+        while (true)
+        {
+            double power = Math.Pow(baseNum, n);
+            if (power > b)
+                break;
+
+            if (power >= a && power <= b)
+                count++;
+
+            baseNum++;
+        }
+
+        return count;
+    }
+}
+
+
--- satestofaili/obj/Debug/net8.0/satestofaili.AssemblyInfo.cs
+++ satestofaili/obj/Debug/net8.0/satestofaili.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("satestofaili")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ec442001e1fac4df78fe6218498c673e4d6405e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bc4f9c86fb37bf5b80040d7f2cd5dfacdf78987")]
 [assembly: System.Reflection.AssemblyProductAttribute("satestofaili")]
 [assembly: System.Reflection.AssemblyTitleAttribute("satestofaili")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
